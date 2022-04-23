4a477b56408e4f7ace92f86bd68cb9757d90957a
Caio Henrique <caio@ganex.com.br>
2022-04-23 01:29:02 -0300

    docs: Adjusting sintax

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -23,7 +23,7 @@ module "security_group_ecs_cluster" {
   version             = "~> 4.9.0"
   name                = "ecs-cluster"
   description         = "ecs-cluster"
-  vpc_id              = module.vpc_ecs-production.vpc_id
+  vpc_id              = module.vpc_ecs_production.vpc_id
   ingress_cidr_blocks = ["0.0.0.0/0"]
   use_name_prefix     = false
   ingress_with_cidr_blocks = [
@@ -58,18 +58,18 @@ module "iam_assumable_role_production_ecsinstancerole" {
   ]
 }
 
-#module "vpc_ecs-production" {
-#  source               = "terraform-aws-modules/vpc/aws"
-#  version              = "3.14.0"
-#  name                 = "ecs-production"
-#  cidr                 = "10.100.0.0/16"
-#  azs                  = ["us-east-1a", "us-east-1c", "us-east-1d"]
-#  public_subnets       = ["10.100.0.0/20", "10.100.16.0/20", "10.100.32.0/20"]
-#  private_subnets      = ["10.100.48.0/20", "10.100.64.0/20", "10.100.80.0/20"]
-#  enable_dns_hostnames = true
-#  enable_dns_support   = true
-#  enable_ipv6          = true
-#}
+module "vpc_ecs-production" {
+  source               = "terraform-aws-modules/vpc/aws"
+  version              = "3.14.0"
+  name                 = "ecs-production"
+  cidr                 = "10.100.0.0/16"
+  azs                  = ["us-east-1a", "us-east-1c", "us-east-1d"]
+  public_subnets       = ["10.100.0.0/20", "10.100.16.0/20", "10.100.32.0/20"]
+  private_subnets      = ["10.100.48.0/20", "10.100.64.0/20", "10.100.80.0/20"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  enable_ipv6          = true
+}
 
 module "autoscalinggroup_nodes_staging" {
   source                    = "terraform-aws-modules/autoscaling/aws"
@@ -81,13 +81,13 @@ module "autoscalinggroup_nodes_staging" {
   image_id                  = data.aws_ami.amazon_linux_ecs.id
   instance_type             = "t3.micro"
   key_name                  = "vpn"
-  security_groups           = [module.security_group_ecs-cluster.security_group_id]
-  iam_instance_profile_name = module.iam_assumable_role_production-ecsinstancerole.iam_instance_profile_id
+  security_groups           = [module.security_group_ecs_cluster.security_group_id]
+  iam_instance_profile_name = module.iam_assumable_role_production_ecsinstancerole.iam_instance_profile_id
   user_data                 = <<-EOT
   #!/bin/bash
   echo "ECS_CLUSTER=${"production"}" >> /etc/ecs/ecs.config
   EOT
-  vpc_zone_identifier       = module.vpc_ecs-production.public_subnets
+  vpc_zone_identifier       = module.vpc_ecs_production.public_subnets
   health_check_type         = "EC2"
   min_size                  = 0
   max_size                  = 1
@@ -106,7 +106,7 @@ module "ecs_capacity_provider" {
   source                    = "ganexcloud/ecs-capacity-provider/aws"
   version                   = "0.0.1"
   name                      = "nodes-staging"
-  auto_scaling_group_arn    = module.autoscalinggroup_nodes-staging.autoscaling_group_arn
+  auto_scaling_group_arn    = module.autoscalinggroup_nodes_staging.autoscaling_group_arn
   target_capacity           = 100
   maximum_scaling_step_size = 4
   minimum_scaling_step_size = 1
